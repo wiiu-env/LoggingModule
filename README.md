6713e33c10785604583731879a2155ad27e4fe68
Maschell <Maschell@gmx.de>
2023-03-17 16:34:51 +0100

    Format via clang format

--- README.md
+++ README.md
@@ -12,3 +12,6 @@ docker run -it --rm -v ${PWD}:/project loggingmodule-builder make
 # make clean
 docker run -it --rm -v ${PWD}:/project loggingmodule-builder make clean
 ```
+## Format the code via docker
+
+`docker run --rm -v ${PWD}:/src ghcr.io/wiiu-env/clang-format:13.0.0-2 -r ./src -i`
--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,13 @@
-#include <wums.h>
 #include <coreinit/debug.h>
+#include <wums.h>
 
 WUMS_MODULE_EXPORT_NAME("homebrew_logging");
 
-#define OSConsoleWriteEx ((void (*)( uint32_t, uint32_t, const char *, size_t))(0x101C400 + 0x10cf0))
+#define OSConsoleWriteEx ((void (*)(uint32_t, uint32_t, const char *, size_t))(0x101C400 + 0x10cf0))
 
 bool WUMSLogWrite(const char *str, size_t size) {
-   OSConsoleWriteEx(0xFFFFFFFF, 0xFFFFFFFF, str, size);
-   return true;
+    OSConsoleWriteEx(0xFFFFFFFF, 0xFFFFFFFF, str, size);
+    return true;
 }
 
 WUMS_EXPORT_FUNCTION(WUMSLogWrite);
